1a4f354f06cf9ce8d4a58710b8b83432a2bf5d7e
Ajmal-hassan <ajmalhassan651@gmail.com>
2020-10-02 12:37:18 +0500

    update 2 checkout controller

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,7 +1,8 @@
 module Spree
   module CheckoutControllerDecorator
     def self.prepended(base)
-      # base.skip_before_action :verify_authenticity_token, :ensure_valid_state
+      # base.before_action :verify_authenticity_token, :ensure_valid_state
+      # before_action :verify_authenticity_token, only: :ensure_valid_state
       base.before_action :two_checkout_hook, :only => [:update]
       base.helper_method :payment_method
     end
@@ -13,7 +14,7 @@ module Spree
     def two_checkout_success
       @order = Order.find_by_number!(params[:cart_order_id])
       two_checkout_validate
-      payment = @order.payments.create(:amount => @order.total, :payment_method_id => payment_method.id)
+      payment = @order.payments.last
       payment.started_processing
       payment.complete!
       @order.state='complete'
@@ -31,24 +32,24 @@ module Spree
      payment_method_id = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
      if payment_method_id.kind_of?(BillingIntegration::TwoCheckout)
        load_order_with_lock
-       @order.payments.create(:amount => @order.total, :payment_method_id => payment_method_id.id, source: payment_method_id)
+       @order.payments.create(:amount => @order.total, :payment_method_id => payment_method_id.id, source: payment_method_id, state: :pending)
        redirect_to(two_checkout_payment_order_checkout_url(@order, :payment_method => payment_method_id))
      end
     end
 
     def two_checkout_validate
-      if payment_method.preferred(:test_mode)
+      if payment_method.preferences[:test_mode]
         order_number = 1
       else
         order_number = params['order_number']
       end
-      if Digest::MD5.hexdigest("#{payment_method.preferred(:secret_word)}#{payment_method.preferred(:sid)}#{order_number}#{'%.2f' % @order.total}").upcase != params['key']
+      if Digest::MD5.hexdigest("#{payment_method.preferences[:secret_word]}#{payment_method.preferences[:sid]}#{order_number}#{'%.2f' % @order.total}").upcase != params['key']
        abort("MD5 Hash did not match. If you are testing with demo sales please select test mode in your payment configuration.")
       end
     end
 
     def payment_method
-      @payment_method ||= PaymentMethod.find(@order.payments.last.payment_method)
+      @payment_method ||= PaymentMethod.find(@order.payments.last.payment_method_id)
     end
   end
 end
